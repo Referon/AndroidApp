5dee9761cbcb9034175bedeff84999705f498e7e
Arseny <senika-2010@mail.ru>
2021-12-25 13:01:08 +1000

    minor fixes

--- app/src/main/java/ru/netology/nmedia/CounterService.kt
+++ app/src/main/java/ru/netology/nmedia/CounterService.kt
@@ -8,36 +8,32 @@ object CounterService {
     private fun counterIntegers(value: Int): String {
         val result = when (value) {
             in 0..999 -> value
-            in 1000..9999 -> value / 1000
-            in 10000..99999 -> value / 1000
-            in 100000..999999 -> value / 1000
-            in 1000000..9999999 -> value / 1000000
+            in 1_000..9_999 -> value / 1_000
+            in 10_000..99_999 -> value / 1_000
+            in 100_000..999_999 -> value / 1_000
+            in 1_000_000..9_999_999 -> value / 1_000_000
 
             else -> 0
         }
         return result.toString()
-
     }
 
     fun counterWithRemains(value: Int): String {
         val valueToFloat = value.toFloat()
-        val re = if (valueToFloat in 1000f..9999f && valueToFloat % 1000 != 0f) {
 
-            value / 1000
-            thousandsAndMillions(valueToFloat / 1000)
+        val re = if (valueToFloat in 1_000f..9_999f && valueToFloat % 1_000 != 0f) {
+            value / 1_000
+            thousandsAndMillions(valueToFloat / 1_000)
 
-        } else if (valueToFloat in 10_000f..99_999f && valueToFloat % 1000 != 0f) {
+        } else if (valueToFloat in 10_000f..99_999f && valueToFloat % 1_000 != 0f) {
+            value / 1_000
+            tensThousands(valueToFloat / 1_000)
 
-            value / 1000
-            tensThousands(valueToFloat / 1000)
-
-        } else if (valueToFloat in 100_000f..999_999f && valueToFloat % 1000 != 0f) {
-
-            value / 1000
-            tensThousands(valueToFloat / 1000)
+        } else if (valueToFloat in 100_000f..999_999f && valueToFloat % 1_000 != 0f) {
+            value / 1_000
+            tensThousands(valueToFloat / 1_000)
 
         } else if (value > 999_999) {
-
             value / 1_000_000
             thousandsAndMillions(valueToFloat / 1_000_000)
 
@@ -65,7 +61,7 @@ object CounterService {
 
     private fun letterTheEnd(value: Int): String {
 
-        val result = if (value in 1000..999_999) {
+        val result = if (value in 1_000..999_999) {
             "K"
         } else if (value > 999_999) {
             "M"
